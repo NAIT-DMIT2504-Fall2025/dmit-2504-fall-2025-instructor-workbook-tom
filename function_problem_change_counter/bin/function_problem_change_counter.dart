9a9f9abf993c74250893ec4b5c17ed67f265b5c0
Tom Maurer <tmaurer@nait.ca>
2025-09-04 10:48:01 -0600

    scaffolded function problem 1

---
+++ function_problem_change_counter/bin/function_problem_change_counter.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+import 'package:function_problem_change_counter/change_helper.dart';
+
+void main(List<String> arguments) {
+  // TODO: Prompt the user for their coins
+
+  // TODO: Calculate the total dollar value
+
+  // TODO: Output the change amount
+}
---
+++ function_problem_change_counter/lib/change_helper.dart
@@ -0,0 +1,29 @@
+// TODO: Create a function that prompts the user for their coin counts
+
+/// Prompts a user for their change counts
+/// [Returns] ordered list of coins (pennies, nickels, dimes, quarters, loonies, toonies)
+List<int> promptForChange() {
+  List<int> output = [];
+
+  // TODO: Prompt for each type of coin
+
+  // TODO: Validate that input is a valid integer
+
+  // TODO: store the value of the coins
+
+  return output;
+}
+
+// TODO: Create a function that calculates change total
+
+/// Calculates the total number of cents given an ordered list of coins (pennies, nickels, dimes, quarters, loonies, toonies)
+/// [Returns] the total number of cents
+int calculateTotal({required List<int> coinCounts}) {
+  int total = 0;
+
+  // TODO: Multiply each coin count by its value
+
+  // TODO: sum coin counts
+
+  return total;
+}
