cc3431224c343c5e52af52fb6f837aa9c4c890e9
Tom Maurer <tmaurer@nait.ca>
2025-09-04 10:50:23 -0600

    completed main function

--- function_problem_change_counter/bin/function_problem_change_counter.dart
+++ function_problem_change_counter/bin/function_problem_change_counter.dart
@@ -3,9 +3,12 @@ import 'dart:io';
 import 'package:function_problem_change_counter/change_helper.dart';
 
 void main(List<String> arguments) {
-  // TODO: Prompt the user for their coins
+  // Prompt the user for their coins
+  var coinCounts = promptForChange();
 
-  // TODO: Calculate the total dollar value
+  // Calculate the total dollar value
+  var total = calculateTotal(coinCounts: coinCounts);
 
-  // TODO: Output the change amount
+  // Output the change amount
+  stdout.writeln('Your total change amount is ${total / 100}');
 }
