fea3a5a8e44c0313143e6af7cec63c143019edd1
Tom Maurer <tmaurer@nait.ca>
2025-09-09 11:19:54 -0600

    finished the example for dog fetching

--- dart_futures_example/bin/dart_futures_example.dart
+++ dart_futures_example/bin/dart_futures_example.dart
@@ -1,7 +1,18 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:dart_futures_example/models/dog.dart';
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) async {
+  try {
+    // Get the dog and parse it
+    var json = await getJson('https://dog.ceo/api/breeds/image/random');
+    var dog = Dog.fromJson(json);
+    print(dog);
+  } catch (e) {
+    // Throw an exception
+    throw Exception('There was an error with the request: ${e.toString()}');
+  }
+}
 
 Future<dynamic> getJson(String endpoint) async {
   var uri = Uri.parse(endpoint); // Create the URI Object from the string
--- dart_futures_example/lib/models/dog.dart
+++ dart_futures_example/lib/models/dog.dart
@@ -5,4 +5,13 @@ class Dog {
   Dog({required String endpoint, required this.status}) {
     imageUri = Uri.parse(endpoint);
   }
+
+  factory Dog.fromJson(dynamic json) {
+    return Dog(endpoint: json['message'], status: json['status']);
+  }
+
+  @override
+  String toString() {
+    return imageUri.toString();
+  }
 }
