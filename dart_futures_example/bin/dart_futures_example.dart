3748bf3d4f6f643eb82f8a333a561a30172caca4
Tom Maurer <tmaurer@nait.ca>
2025-09-09 10:47:40 -0600

    initial json request function

--- dart_futures_example/bin/dart_futures_example.dart
+++ dart_futures_example/bin/dart_futures_example.dart
@@ -1,1 +1,11 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 void main(List<String> arguments) {}
+
+Future<dynamic> getJson(String endpoint) async {
+  var uri = Uri.parse(endpoint); // Create the URI Object from the string
+  var response = await http.get(uri); // Make the http request
+
+  return jsonDecode(response.body);
+}
