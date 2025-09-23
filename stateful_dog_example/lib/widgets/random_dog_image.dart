fd9740ff2f26bf845cbcebd7dc5fe06800e5ec24
Tom Maurer <tmaurer@nait.ca>
2025-09-23 11:08:14 -0600

    loading a random dog image on start

--- stateful_dog_example/lib/widgets/random_dog_image.dart
+++ stateful_dog_example/lib/widgets/random_dog_image.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class RandomDogImage extends StatefulWidget {
   const RandomDogImage({super.key});
+
+  final String endpoint = 'https://dog.ceo/api/breeds/image/random';
 
   @override
   State<RandomDogImage> createState() => _RandomDogImageState();
 }
 
 class _RandomDogImageState extends State<RandomDogImage> {
-  String _dogImageUrl =
-      'https://images.dog.ceo/breeds/mudhol-indian/Indian-Mudhol.jpg';
+  String _dogImageUrl = '';
+
+  Future<String> getRandomDogURL() async {
+    var endpoint = Uri.parse(widget.endpoint);
+    var response = await http.get(endpoint);
+    return jsonDecode(response.body)['message'];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Get a random dog on load
+    getRandomDogURL().then((url) {
+      // Set the state to trigger a rerender
+      setState(() {
+        _dogImageUrl = url;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
