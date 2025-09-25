5edee6fb9f727e032ad1bc19f0f7f1e6ed5cdab0
Tom Maurer <tmaurer@nait.ca>
2025-09-25 10:39:23 -0600

    Step 2 complete

--- basic_forms_example/lib/main.dart
+++ basic_forms_example/lib/main.dart
@@ -10,25 +10,39 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Login Form'),
-                TextField(decoration: InputDecoration(label: Text('Username'))),
-                TextField(
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: InputDecoration(label: Text('Password')),
-                ),
-                TextButton(onPressed: () {}, child: Text('Submit')),
-              ],
+      home: Scaffold(body: Center(child: UserLoginForm())),
+    );
+  }
+}
+
+class UserLoginForm extends StatefulWidget {
+  const UserLoginForm({super.key});
+  @override
+  State<UserLoginForm> createState() => _UserLoginFormState();
+}
+
+class _UserLoginFormState extends State<UserLoginForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Login Form'),
+            TextFormField(decoration: InputDecoration(label: Text('Username'))),
+            TextFormField(
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration: InputDecoration(label: Text('Password')),
             ),
-          ),
+            TextButton(onPressed: () {}, child: Text('Submit')),
+          ],
         ),
       ),
     );
