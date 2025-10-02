24a02eb1d9311c7a5d95adc4e2cad3c4888e1a9b
Tom Maurer <tmaurer@nait.ca>
2025-10-02 10:54:55 -0600

    Basic settings manager setup

--- nested_nav/lib/pages/settings/settings_manager.dart
+++ nested_nav/lib/pages/settings/settings_manager.dart
@@ -8,8 +8,41 @@ class SettingsManager extends StatefulWidget {
 }
 
 class _SettingsManagerState extends State<SettingsManager> {
+  Route _customOnGenerateRoute(RouteSettings settings) {
+    Widget page;
+
+    switch (settings.name) {
+      case '/home':
+        // TODO: Assign page to the settings home page
+        page = Text('home');
+        break;
+      case '/option1':
+        // TODO: Assign page to the option 1 page
+        page = Text('o 1');
+        break;
+      case '/option2':
+        // TODO: Assign page to the option 2 page
+        page = Text('o 2');
+        break;
+      case '/option3':
+        page = Text('o 3');
+        // TODO: Assign page to the option 3 page
+        break;
+      default:
+        page = Text('default');
+        break;
+    }
+
+    return MaterialPageRoute(builder: (context) => page);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("This is the settings manager"));
+    return Scaffold(
+      body: Navigator(
+        onGenerateRoute: _customOnGenerateRoute,
+        initialRoute: '/home',
+      ),
+    );
   }
 }
