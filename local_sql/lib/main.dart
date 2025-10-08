507e797234c0159687746b44fd79eec5745b577a
Tom Maurer <tmaurer@nait.ca>
2025-10-08 08:43:27 -0600

    frontend list tiles

--- local_sql/lib/main.dart
+++ local_sql/lib/main.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:local_sql/models/dog.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  List<Dog> dogs = [
+    Dog(id: 0, name: 'Quinn', age: 6),
+    Dog(id: 1, name: 'Shinki', age: 2),
+    Dog(id: 87, name: 'Crosby', age: 8),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: ListView.separated(
+            itemBuilder: (context, index) {
+              return ListTile();
+            },
+            separatorBuilder: (_, __) {
+              return Divider();
+            },
+            itemCount: dogs.length,
+          ),
         ),
       ),
     );
