5dfa5dc3abc4fd00d5892d2e841e69e29a3a7fb3
Tom Maurer <tmaurer@nait.ca>
2025-10-14 11:44:23 -0600

    initial firebase example

---
+++ dmit_2504_f2025/README.md
@@ -0,0 +1,3 @@
+# dmit_2504_f2025
+
+A new Flutter project.
---
+++ dmit_2504_f2025/android/app/src/main/kotlin/com/example/dmit_2504_f2025/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dmit_2504_f2025
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
