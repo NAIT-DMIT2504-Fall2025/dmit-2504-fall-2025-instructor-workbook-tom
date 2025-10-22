cdc1042eb7a96a5783cae1511cf3b60f2aef67bd
Tom Maurer <tmaurer@nait.ca>
2025-10-22 08:53:50 -0600

    Finished Inherited Notifier

--- app_state/app_state_inherited_notifier/lib/pages/last_name_page.dart
+++ app_state/app_state_inherited_notifier/lib/pages/last_name_page.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
 
+import 'package:app_state_inherited_notifier/widgets/user_notifier.dart';
+import 'package:app_state_inherited_notifier/models/user.dart';
+
 class LastNamePage extends StatelessWidget {
   const LastNamePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    User user = UserNotifier.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Last Name Page'),
-      ),
-      body: const Center(
+      appBar: AppBar(title: const Text('Last Name Page')),
+      body: Center(
         child: Column(
           children: <Widget>[
-            SizedBox(
-              height: 24.0,
+            SizedBox(height: 24.0),
+            ListenableBuilder(
+              listenable: user,
+              builder: (context, child) {
+                return Text('User name: ${user.firstName} ${user.lastName}');
+              },
             ),
-            Text('User name: USER_NAME HERE'),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         // Update our user last name here
         onPressed: () {
-          //noop;
+          user.lastName = 'Weaver';
         },
         child: const Icon(Icons.update),
       ),
